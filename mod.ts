02bc5216cd4d40cd02ab61e84940df7dd63d40a9
Xiaokai Li <mike.lxk@gmail.com>
2021-02-16 04:53:03 +0800

    add fetch arrayBuffer

--- mod.ts
+++ mod.ts
@@ -17,7 +17,7 @@ export const fetchJson = async (
     return await res.json();
   } catch (error) {
     throw new Error(
-      `Can not parse json${error}`,
+      `Can not parse json : ${error}`,
     );
   }
 };
@@ -30,3 +30,26 @@ export const fetchJsonData = async (
   input: string | Request | URL,
   init?: RequestInit | undefined,
 ) => (await fetchJson(input, init)).data;
+/**
+ * @param input input url or request
+ * @param init init args
+ * @returns `response.arrayBuffer()`
+ */
+export const fetchBuffer = async (
+  input: string | Request | URL,
+  init?: RequestInit | undefined,
+) => {
+  const res = await fetch(input, { ...init });
+  if (!res.ok) {
+    throw new Error(
+      `Response not okay with code${res.status}:,${res.statusText}`,
+    );
+  }
+  try {
+    return await res.arrayBuffer();
+  } catch (error) {
+    throw new Error(
+      `Can not parse response body as ArrayBuffer${error}`,
+    );
+  }
+};
